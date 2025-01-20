60b77bc74dc83759708e337a276386e29a74a325
Wahid Shaikh <wahid.shaikh@dynacommerce.com>
2025-01-20 12:06:27 +0530

    Vite project setup

---
+++ src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+  <teleport to=".modals-container">
+    <div class="modal">
+      <h1>This is a modal</h1>
+      <p>Loren ipsum color dfdf afdfaf afss asfa fa sdbskdbfs</p>
+      <button>Hide modal</button>
+    </div>
+  </teleport>
+</template>
+
+<style>
+.modal {
+  background: beige;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+}
+</style>
---
+++ src/views/ModalsView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="modals">
+    <h1>Modals</h1>
+    <button @click="showModal = true">Show modal</button>
+    <Modal v-if="showModal"></Modal>
+  </div>
+</template>
+
+<script setup>
+/*
+    imports
+*/
+
+import { ref } from "vue";
+import Modal from "@/components/Modal.vue";
+
+/*
+    modals
+*/
+const showModal = ref(false);
+</script>
+
+<!--
+<script>
+import Modal from "@/components/Modal.vue";
+export default {
+  components: {
+    Modal,
+  },
+};
+</script>
+-->
